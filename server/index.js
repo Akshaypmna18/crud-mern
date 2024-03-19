6a97bbdea9afa10a89c0aca29420755d6d495b0d
Aкѕнαу <akshaypmnacoc@gmail.com>
2024-03-19 21:38:23 +0530

    feat: complete server side

--- server/index.js
+++ server/index.js
@@ -1,1 +1,26 @@
-console.log("hi");
+const express = require("express");
+const ProductRoutes = require("./routes/product.route.js");
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+const app = express();
+
+const PORT = process.env.PORT;
+const DB_PASSWORD = process.env.DB_PASSWORD;
+
+mongoose
+  .connect(
+    `mongodb+srv://admin:${DB_PASSWORD}@crud-mern-db.0gw13jl.mongodb.net/crud-API?retryWrites=true&w=majority&appName=crud-mern-db`
+  )
+  .then(() => {
+    console.log("Connected!");
+    app.listen(PORT, () => console.log(`running on ${PORT}`));
+  })
+  .catch((err) => console.log(`Connection failed : ${err.message}`));
+
+//middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// routes
+app.use("/products", ProductRoutes);
