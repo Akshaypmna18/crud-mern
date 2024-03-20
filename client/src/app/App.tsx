f14864800efcb5c16bc6de14533cd4647aaf2765
Aкѕнαу <akshaypmnacoc@gmail.com>
2024-03-20 10:29:00 +0530

    chore: test API

--- client/src/app/App.tsx
+++ client/src/app/App.tsx
@@ -6,9 +6,19 @@ import axios from "axios";
 const API_URL = process.env.NEXT_PUBLIC_API_URL;
 
 const getProducts = async () => {
-  const response = await axios.get(`${API_URL}/products`);
+  const response = await axios.get(`${API_URL}`);
   console.log(response);
 };
+// const getProduct = async () => {
+//   const response = await axios.get(
+//     `${API_URL}/65f9a8af693e1f025eac90ba`
+//   );
+//   console.log(response);
+// };
+// const deleteProduct = async () => {
+//   const response = await axios.delete(`${API_URL}/65f9a8af693e1f025eac90ba`);
+//   console.log(response);
+// };
 
 export default function Home() {
   useEffect(() => {
