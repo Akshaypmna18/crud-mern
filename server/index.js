62e6a09d94fead35891ea269e6f69849fd9234be
Aкѕнαу <akshaypmnacoc@gmail.com>
2024-03-20 10:40:32 +0530

    feat: update header

--- server/index.js
+++ server/index.js
@@ -32,6 +32,7 @@ app.use((req, res, next) => {
   if (allowedOrigins.includes(origin)) {
     res.setHeader("Access-Control-Allow-Origin", origin);
   }
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
