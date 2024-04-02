e5b654de0480e3cd30d54bae4d2ab06959e79a68
Aкѕнαу <akshaypmnacoc@gmail.com>
2024-04-02 18:24:45 +0530

    feat: complete form validation for add product

--- client/src/components/DialogModal.tsx
+++ client/src/components/DialogModal.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { Dialog, DialogContent } from "@/components/ui/dialog";
 import { Context } from "@/context";
 import { Dispatch, SetStateAction, useContext, useState } from "react";
@@ -16,11 +18,38 @@ import { commonAPI } from "@/lib/services";
 import { useToast } from "@/components/ui/use-toast";
 import { Loader } from "./Loader";
 import ImageUpload from "./ImageUpload";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { z } from "zod";
+import { useEffect } from "react";
 
 export type ModalProps = {
   open: boolean;
   setIsOpen: Dispatch<SetStateAction<boolean>>;
 };
+
+export const updateSchema = z.object({
+  name: z
+    .string()
+    .min(3, "Name must be at least 3 characters long")
+    .max(50, "Name cannot exceed 50 characters"),
+  price: z.coerce
+    .number({
+      required_error: "Price is required",
+      invalid_type_error: "Price must be a number",
+    })
+    .gte(0, "Price must be greater than or equal to 0"),
+  quantity: z.coerce
+    .number({
+      required_error: "Quantity is required",
+      invalid_type_error: "Quantity must be a number",
+    })
+    .gte(0, "Quantity must be greater than or equal to 0"),
+  image: z
+    .string({
+      required_error: "URL is required",
+    })
+    .url("Image must be a valid URL"),
+});
 
 export default function DialogModal({ open, setIsOpen }: ModalProps) {
   const { currentProduct, refetchProducts, image, setImage } =
@@ -29,12 +58,13 @@ export default function DialogModal({ open, setIsOpen }: ModalProps) {
   const { toast } = useToast();
   const form = useForm({
     mode: "onChange",
+    resolver: zodResolver(updateSchema),
   });
   const updateProduct = async (data: any) => {
     try {
       const validData = {
         name: data.name || currentProduct.name,
-        image: image?.[0]?.cdnUrl || currentProduct.image,
+        image: data.image || currentProduct.image,
         price: data.price || currentProduct.price,
         quantity: data.quantity || currentProduct.quantity,
       };
@@ -42,7 +72,7 @@ export default function DialogModal({ open, setIsOpen }: ModalProps) {
       await commonAPI(currentProduct._id, "PUT", validData);
       toast({
         variant: "success",
-        title: "Product Added Successfully",
+        title: "Product Updated Successfully",
         duration: 2500,
       });
       refetchProducts();
@@ -60,15 +90,22 @@ export default function DialogModal({ open, setIsOpen }: ModalProps) {
       setImage(undefined);
     }
   };
+  useEffect(() => {
+    form.setValue("image", image?.[0]?.cdnUrl);
+  }, [image?.[0]?.cdnUrl]);
   return (
     <Dialog
       open={open}
       onOpenChange={() => {
         setIsOpen(!open);
         form.reset();
+        setImage(undefined);
       }}
     >
-      <DialogContent>
+      <DialogContent
+        onInteractOutside={(e) => e.preventDefault()}
+        onOpenAutoFocus={(e) => e.preventDefault()}
+      >
         <Form {...form}>
           <form
             onSubmit={form.handleSubmit(updateProduct)}
@@ -104,7 +141,7 @@ export default function DialogModal({ open, setIsOpen }: ModalProps) {
                   </FormLabel>
                   <FormControl>
                     <Input
-                      type="text"
+                      type="number"
                       placeholder="Enter Product Price"
                       defaultValue={currentProduct.price}
                       {...field}
